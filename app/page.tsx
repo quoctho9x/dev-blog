e707261df8661d60b1f5397db144bc3d9b3199a3
thonguyenquoc <tho.nguyen3@tiki.vn>
2023-11-08 15:49:53 +0700

    move page to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,8 @@
-import { getBlogs } from "@/server/blogs";
-import ListBlog from "../components/ListBlog";
-import Filter from "@/components/Filter";
+import Home from "@/components/Home";
 
-export default function Home() {
-  // const blogs = await getBlogs();
-  // let tags: string[] = [];
-  // for (const blog of blogs) {
-  //   for (const tag of blog.tags) {
-  //     if (!tags.includes(tag)) {
-  //       tags.push(tag);
-  //     }
-  //   }
-  // }
+export default function Page() {
 
   return (
-    <main className="w-screen h-screen overflow-auto flex flex-col items-center bg-zinc-800 text-neutral-300">
-      <title>Home page</title>
-      <section>
-        <div className="mt-3 text-center">
-          <h1 className="text-[3rem]">welcome to dev blog a</h1>
-          <p>
-            {" "}
-            A full-stack blog made with next.js, tailwindCSS, Github GraphQL
-          </p>
-          {/* <Filter tags={tags} /> */}
-          {/* <ListBlog blogs={blogs} /> */}
-          {/* <p>{JSON.stringify(blogs)}</p> */}
-        </div>
-      </section>
-    </main>
+    <Home />  
   );
 }
